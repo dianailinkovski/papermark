5efffc65e6a6ea171252ff9df49ade0a1b948c7e
osipestar <naruto991223@gmail.com>
2023-12-02 18:13:49 +0100

    update 123

--- app/(static)/open-source-investors/page.tsx
+++ app/(static)/open-source-investors/page.tsx
@@ -47,9 +47,9 @@ const getInvestors = async () => {
       },
     },
   );
-  if (!response.ok) {
-    throw new Error("Network response was not ok " + response.statusText);
-  }
+  // if (!response.ok) {
+  //   throw new Error("Network response was not ok " + response.statusText);
+  // }
   const data = await response.json();
   return data;
 };
